4d8ba71cc033ad3d1dbce6aed2c76e0c16d1f06f
TRSTN4 <TRSTN4@users.noreply.github.com>
2020-06-18 22:42:27 +0200

    Renamed some functions and variables

--- assets/banners.py
+++ assets/banners.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 from assets.properties import version
+from assets.properties import total_scripts
 
 payloads_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Payloads | [Total] 0 Payloads Available'
 listeners_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Listeners | [Total] 0 Listeners Available'
-scripts_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Total] 2 Scripts Available'
+scripts_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Total] ' + total_scripts +' Scripts Available'
 scanning_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Scanning'
 enumeration_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Enumeration'
 privilege_escalation_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Privilege Escalation'
-other_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Other'
-rsgen_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Other | [Script] RSGEN'
-machanger_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Other | [Script] MACHANGER'
+miscellaneous_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Miscellaneous'
+rsgen_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Exploitation | [Script] RSGEN'
+machanger_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Miscellaneous | [Script] MACHANGER'
 eagleye_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Scanning | [Script] EAGLEYE'
 eaglscan_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Scanning | [Script] EAGLESCAN'
-pgen_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Other | [Script] PGEN'
+pgen_banner = '\u001b[36;1m [Version] ' + version + ' | [Module] Eagle Scripts | [Option] Exploitation | [Script] PGEN'
