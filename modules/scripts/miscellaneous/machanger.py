8f93b55aef71fa11374682c13df21afb034eea97
TRSTN4 <TRSTN4@users.noreply.github.com>
2020-06-21 23:18:16 +0200

    Release MACHANGER Script

--- modules/scripts/miscellaneous/machanger.py
+++ modules/scripts/miscellaneous/machanger.py
@@ -1,44 +1,158 @@
-#!/usr/bin.env python
+#!/usr/bin/python3
 
+# Imports all the needed variables and packages
+from assets.banners import machanger_banner
+from assets.designs import *
+from assets.properties import clear_screen
 import subprocess
-import optparse
+import netifaces
+import os
 import re
 
 
-def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    (options, arguments) = parser.parse_args()
-    if not options.interface:
-        parser.error("[-] Please specify an interface, use --help for more info")
-    elif not options.new_mac:
-        parser.error("[-] Please specify a new mac, use --help for more info")
-    return options
+# Main function
+def machanger_main():
 
-def change_mac(interface, new_mac):
-    print("[+] Changing MAC address for " + interface + " to " + new_mac)
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    # Function that takes interface input
+    def interface():
+        try:
+            global interface_set
+            os.system(clear_screen)
+            print(logo)
+            print('')
+            print(line)
+            print(machanger_banner)
+            print(line)
+            print('')
+            print(author)
+            print('Interface:')
+            ips()
+            print('')
+            while True:
+                interface_set = input('\u001b[33mINTERFACE \u001b[37m> ').lower()
+                if interface_set == 'wlan0' or interface_set == 'wlan1' or interface_set == 'wlan2' or interface_set == 'wlan3' or interface_set == 'mon0' or interface_set == 'mon1' or interface_set == 'mon2' or interface_set == 'mon3' or interface_set == 'wlp5s0' or interface_set == 'wlp5s1' or interface_set == 'wlp5s2' or interface_set == 'wlp5s3' or interface_set == 'eth0' or interface_set == 'eth1' or interface_set == 'eth2' or interface_set == 'eth3':
+                    mac()
+                else:
+                    print('\u001b[31m[-] Invalid Interface.')
+                    continue
+        except KeyboardInterrupt:
+            exit_shell()
 
-def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-    if mac_address_search_result:
-        return mac_address_search_result.group(0)
-    else:
-        print("[-] Could not read MAC address.")
+    # Function that takes mac input
+    def mac():
+        try:
+            global mac_set
+            os.system(clear_screen)
+            print(logo)
+            print('')
+            print(line)
+            print(machanger_banner)
+            print(line)
+            print('')
+            print(author)
+            print('MAC Address:')
+            print('')
+            print('\tExample 1: 00:11:22:33:44:55')
+            print('\tExample 2: 12:22:33:44:55:66')
+            print('')
+            while True:
+                mac_set = input('\u001b[33mNEW MAC \u001b[37m> ').lower()
+                print('')
+                functions()
+        except KeyboardInterrupt:
+            exit_shell()
 
-options = get_arguments()
+    # Function that shows all available and unavailable interfaces
+    def ips():
+        x = netifaces.interfaces()
 
-current_mac = get_current_mac(options.interface)
-print("Current MAC = " + str(current_mac))
+        for i in x:
+            if i == 'wlan0' or i == 'wlan1' or i == 'wlan2' or i == 'wlan3' or i == 'mon0' or i == 'mon1' or i == 'mon2' or i == 'mon3' or i == 'wlp5s0' or i == 'wlp5s1' or i == 'wlp5s2' or i == 'wlp5s3' or i == 'eth0' or i == 'eth1' or i == 'eth2' or i == 'eth3':
+                print('\n\t[+] Available Interface: ' + i)
+            elif i != 'lo' or i != 'tun0' or i != 'tun1' or i != 'tun2' or i != 'tun3':
+                print('\n\t[-] Unavailable Interface: ' + i)
 
-change_mac(options.interface, options.new_mac)
+    # Function that changes MAC address
+    def change_mac():
+        os.system("ifconfig " + interface_set + " down" + " >/dev/null 2>&1")
+        os.system("ifconfig " + interface_set + " hw " + " ether " + mac_set + " >/dev/null 2>&1")
+        os.system("ifconfig " + interface_set + " up" + " >/dev/null 2>&1")
 
-current_mac = get_current_mac(options.interface)
-if current_mac == options.new_mac:
-    print("[+] MAC address was successfully changed to " + current_mac)
-else:
-    print("[-] MAC address did not get changed.")
+    # function that checks MAC address
+    def get_current_mac():
+        try:
+            ifconfig_result = subprocess.check_output(["ifconfig", interface_set])
+            mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+            if mac_address_search_result:
+                return mac_address_search_result.group(0)
+            else:
+                pass
+        except subprocess.CalledProcessError:
+            print('print')
+
+    # Function that executes other functions
+    def functions():
+        global worked_or_not
+        global current_mac
+        global color
+        current_mac = get_current_mac()
+
+        change_mac()
+
+        current_mac = get_current_mac()
+        if current_mac == mac_set:
+            worked_or_not = "\u001b[32;1mMAC address was successfully changed."
+            color = '\u001b[32;1m'
+            result()
+        else:
+            worked_or_not = "\u001b[31mMAC address did not get changed."
+            color = '\u001b[31m'
+            result()
+
+    # Function that displays result
+    def result():
+        try:
+            os.system(clear_screen)
+            print(logo)
+            print('')
+            print(line)
+            print(machanger_banner)
+            print(line)
+            print('')
+            print(author)
+            print('Output:')
+            print('')
+            print('\tINTERFACE SET: ' + interface_set)
+            print('')
+            print('\tMAC SET: ' + mac_set)
+            print('')
+            print('\t---------------------------------------------')
+            print('')
+            print('\tRESULT: ' + worked_or_not + color + '\u001b[37m')
+            print('')
+            print('\tCURRENT MAC: ' + color + current_mac + color + '\u001b[37m')
+            print('')
+            print('\t1): New')
+            print('\t2): Exit')
+            print('')
+            while True:
+                eagleshell_cmd = input('\u001b[33mEagleShell \u001b[37m> ').lower()
+                if eagleshell_cmd == '1':
+                    machanger_main()
+                elif eagleshell_cmd == '2':
+                    exit_shell()
+                else:
+                    print('\u001b[31m[-] Invalid Input.')
+                    continue
+        except KeyboardInterrupt:
+            exit_shell()
+
+    # Function that exits
+    def exit_shell():
+        from assets.functions import exit_main
+        exit_main()
+
+    interface()
+
+
+machanger_main()
