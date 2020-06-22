7c6a344e705e53c7643992751c94205a417e1d78
TRSTN4 <TRSTN4@users.noreply.github.com>
2020-06-22 20:55:15 +0200

    Version degrade

--- assets/properties.py
+++ assets/properties.py
@@ -2,11 +2,10 @@
 
 # How the version structure works.
 total_mass_updates = '1'
-total_modules = '2'
+total_modules = '1'
 total_categorys = '6'
 total_scripts = '7'
 version = total_mass_updates + '.' + total_modules + '.' + total_categorys + '.' + total_scripts
 
 # Variables that are used a lot.
 clear_screen = 'clear'
-
